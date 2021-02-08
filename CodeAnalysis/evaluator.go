790ca53e198e7a3fe5be80f697f61eaf8e57a6eb
Petru <smart__petea@mail.ru>
2021-02-08 17:52:37 +0200

    #5 52:24

--- CodeAnalysis/evaluator.go
+++ CodeAnalysis/evaluator.go
@@ -6,6 +6,7 @@ import (
     Binding "minsk/CodeAnalysis/Binding"
     "minsk/CodeAnalysis/Binding/Kind/BoundUnaryOperatorKind"
     "minsk/CodeAnalysis/Binding/Kind/BoundBinaryOperatorKind"
+    "minsk/CodeAnalysis/Binding/Kind/BoundNodeKind"
     "minsk/Util"
 
     "reflect"
@@ -28,76 +29,89 @@ func (e *Evaluator) Evaluate() interface{} {
 }
 
 func (e *Evaluator) evaluateExpression(root Binding.BoundExpression) interface{} {
-    if l, ok := root.(*Binding.BoundLiteralExpression); ok {
-        return l.Value
-    }
-
-    if v, ok := root.(*Binding.BoundVariableExpression); ok {
-        return e._variables[v.Variable]
-    }
-
-    if a, ok := root.(*Binding.BoundAssignmentExpression); ok {
-        value := e.evaluateExpression(a.Expression)
-        e._variables[a.Variable] = value
-        return value
-    }
-
-    if u, ok := root.(*Binding.BoundUnaryExpression); ok {
-        operand := e.evaluateExpression(u.Operand)
-
-        switch u.Op.Kind {
-        case BoundUnaryOperatorKind.Identity:
-            return operand.(int)
-        case BoundUnaryOperatorKind.Negation:
-            return -(operand.(int))
-        case BoundUnaryOperatorKind.LogicalNegation:
-            return !(operand.(bool))
+    switch root.Kind() {
+        case BoundNodeKind.LiteralExpression:
+            return e.evaluateLiteralExpression(root.(*Binding.BoundLiteralExpression))
+        case BoundNodeKind.VariableExpression:
+            return e.evaluateVariableExpression(root.(*Binding.BoundVariableExpression))
+        case BoundNodeKind.AssignmentExpression:
+            return e.evaluateAssignmentExpression(root.(*Binding.BoundAssignmentExpression))
+        case BoundNodeKind.UnaryExpression:
+            return e.evaluateUnaryExpression(root.(*Binding.BoundUnaryExpression))
+        case BoundNodeKind.BinaryExpression:
+            return e.evaluateBinaryExpression(root.(*Binding.BoundBinaryExpression))
         default:
-            panic(fmt.Sprintf("Unexpected unary operator %s", u.Op.Kind))
-        }
+            panic(fmt.Sprintf("Unexpected node %s", root.Kind()))
     }
+}
 
-    if b, ok := root.(*Binding.BoundBinaryExpression); ok {
-        left := e.evaluateExpression(b.Left)
-        right := e.evaluateExpression(b.Right)
+func (e *Evaluator) evaluateLiteralExpression(l *Binding.BoundLiteralExpression) interface{} {
+    return l.Value
+}
 
-        switch b.Op.Kind {
-        case BoundBinaryOperatorKind.Addition:
-            return left.(int) + right.(int)
+func (e *Evaluator) evaluateVariableExpression(v *Binding.BoundVariableExpression) interface{} {
+    return e._variables[v.Variable]
+}
 
-        case BoundBinaryOperatorKind.Subtraction:
-            return left.(int) - right.(int)
+func (e *Evaluator) evaluateAssignmentExpression(a *Binding.BoundAssignmentExpression) interface{} {
+    value := e.evaluateExpression(a.Expression)
+    e._variables[a.Variable] = value
+    return value
+}
 
-        case BoundBinaryOperatorKind.Multiplication:
-            return left.(int) * right.(int)
+func (e *Evaluator) evaluateUnaryExpression(u *Binding.BoundUnaryExpression) interface{} {
+    operand := e.evaluateExpression(u.Operand)
 
-        case BoundBinaryOperatorKind.Division:
-            return left.(int) / right.(int)
-
-        case BoundBinaryOperatorKind.LogicalAnd:
-            return left.(bool) && right.(bool)
-
-        case BoundBinaryOperatorKind.LogicalOr:
-            return left.(bool) || right.(bool)
-
-        case BoundBinaryOperatorKind.Equals:
-            if b.Left.GetType() == reflect.Bool {
-                return left.(bool) == right.(bool)
-            } else {
-                return left.(int) == right.(int)
-            }
-
-        case BoundBinaryOperatorKind.NotEquals:
-            if b.Left.GetType() == reflect.Bool {
-                return left.(bool) != right.(bool)
-            } else {
-                return left.(int) != right.(int)
-            }
-
-        default:
-            panic(fmt.Sprintf("Unexpected binary operator %s", b.Op.Kind))
-        }
+    switch u.Op.Kind {
+    case BoundUnaryOperatorKind.Identity:
+        return operand.(int)
+    case BoundUnaryOperatorKind.Negation:
+        return -(operand.(int))
+    case BoundUnaryOperatorKind.LogicalNegation:
+        return !(operand.(bool))
+    default:
+        panic(fmt.Sprintf("Unexpected unary operator %s", u.Op.Kind))
     }
+}
 
-    panic(fmt.Sprintf("Unexpected node %s", root.Kind()))
+func (e *Evaluator) evaluateBinaryExpression(b *Binding.BoundBinaryExpression) interface{} {
+    left := e.evaluateExpression(b.Left)
+    right := e.evaluateExpression(b.Right)
+
+    switch b.Op.Kind {
+    case BoundBinaryOperatorKind.Addition:
+        return left.(int) + right.(int)
+
+    case BoundBinaryOperatorKind.Subtraction:
+        return left.(int) - right.(int)
+
+    case BoundBinaryOperatorKind.Multiplication:
+        return left.(int) * right.(int)
+
+    case BoundBinaryOperatorKind.Division:
+        return left.(int) / right.(int)
+
+    case BoundBinaryOperatorKind.LogicalAnd:
+        return left.(bool) && right.(bool)
+
+    case BoundBinaryOperatorKind.LogicalOr:
+        return left.(bool) || right.(bool)
+
+    case BoundBinaryOperatorKind.Equals:
+        if b.Left.GetType() == reflect.Bool {
+            return left.(bool) == right.(bool)
+        } else {
+            return left.(int) == right.(int)
+        }
+
+    case BoundBinaryOperatorKind.NotEquals:
+        if b.Left.GetType() == reflect.Bool {
+            return left.(bool) != right.(bool)
+        } else {
+            return left.(int) != right.(int)
+        }
+
+    default:
+        panic(fmt.Sprintf("Unexpected binary operator %s", b.Op.Kind))
+    }
 }
