a9a142bcdb3f4d1c44b16ba491ca717be5775ca1
Petru <smart__petea@mail.ru>
2021-03-06 11:29:29 +0200

    #8 1:31:18

--- CodeAnalysis/Lowering/lowerer.go
+++ CodeAnalysis/Lowering/lowerer.go
@@ -2,6 +2,8 @@ package Lowering
 
 import (
     "minsk/CodeAnalysis/Binding"
+    SyntaxKind "minsk/CodeAnalysis/Syntax/Kind"
+    "reflect"
 )
 
 type Lowerer struct {
@@ -16,4 +18,44 @@ func LowererLower(statement Binding.BoundStatement) Binding.BoundStatement {
     lowerer := newLowerer()
 
     return lowerer.RewriteStatement(statement)
+}
+
+func (l *Lowerer) RewriteForStatement(node *BoundForStatement) Binding.BoundStatement {
+    //for i = <lower> to <upper>
+    //      <body>
+    //
+    // ---->
+    //{
+    //  var <var> = <lower>
+    //  while (<var> <= <upper>)
+    //  {
+    //     <body>
+    //     <var> = <var> + 1
+    //  }
+    //}
+
+    variableDeclaration := Binding.NewBoundVariableDeclaration(node.Variable, node.LowerBound)
+    variableExpression := Binding.NewBoundVariableExpression(node.Variable)
+    condition := Binding.NewBoundBinaryExpression(
+        variableExpression,
+        Binding.BoundBinaryOperator.Bind(SyntaxKind.LessOrEqualsToken, reflect.Int, reflect.Int),
+        node.UpperBound,
+    )
+
+    increment := Binding.NewBoundExpressionStatement(
+        Binding.NewBoundAssignmentExpression(
+            node.Variable,
+            Binding.NewBoundBinaryExpression(
+                variableExpression,
+                Binding.BoundBinaryOperator.Bind(SyntaxKind.PlusToken, reflect.Int, reflect.Int),
+                Binding.NewBoundLiteralExpression(1),
+            ),
+        )
+    )
+
+    whileBody := Binding.NewBoundBlockStatement([]BoundStatement{node.Body, increment})
+    whileStatement := Binding.NewBoundWhileStatement(condition, whileBody)
+    result := Binding.NewBoundBlockStatement([]BoundStatement{variableDeclaration, whileStatement})
+
+    return l.RewriteStatement(result)
 }
